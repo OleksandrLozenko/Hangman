b82a861365c9ba35b3db51774dd7d81bc265549b
Oleksandr Lozenko <131644673+OleksandrLozenko@users.noreply.github.com>
2023-05-19 11:08:32 +0300

    Update words.py

--- words.py
+++ words.py
@@ -1,10 +1,8 @@
 import pygame
 pygame.init()
 font = pygame.font.SysFont('Arial', 50)
-menu_language, choose_language,remaining_tries  = 'Язык', 'Выберите язык', 'Попытки'
+menu_language, choose_language,remaining_tries,choose_topic  = 'Язык', 'Выберите язык', 'Попытки','Выберите тему'
 victory = 'Молодец!'
-game_over = 'Вы проиграли!'
-game = 'Вы угадали!'
 language_surface = font.render(menu_language, True, (0, 0, 0))
 choose_language_surface = font.render(choose_language, True, (0, 0, 0))
 ALPHABET_RUS = ["А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"]
@@ -27,7 +25,7 @@ WORDS_TRANSPORT = {
 }
 
 WORDS_IT = {
-    0: ["Программирование", "Алгоритм", "Кодирование", "Разработка", "Веб-сайт", "База данных", "Интернет", "Компьютер", "Сервер", "Кибербезопасность"],
+    0: ["Программирование", "Алгоритм", "Кодирование", "Разработка", "Веб-сайт", "Код", "Интернет", "Компьютер", "Сервер", "Кибербезопасность"],
     1: ["Programming", "Algorithm", "Coding", "Development", "Website", "Database", "Internet", "Computer", "Server", "Cybersecurity"],
     2: ["Programmeerimine", "Algoritm", "Kodeerimine", "Arendus", "Veebileht", "Andmebaas", "Internet", "Arvuti", "Server", "Küberkaitse"]
 }
