cd0f08b0ef02a3cc1d006a3845af8aafe8c488f9
Oleksandr Lozenko <131644673+OleksandrLozenko@users.noreply.github.com>
2023-05-28 14:57:50 +0300

    Update settings.py
    Update settings

--- settings.py
+++ settings.py
@@ -14,12 +14,10 @@ TEXTBOX_WIDTH = 150
 TEXTBOX_HEIGHT = 40
 COlOR_ENCRYPTED_WORD = (0, 0, 0)
 
-# Слова
-WORDS = WORDS_IT # Слова по умолчанию
-ALPHABET = ALPHABET_RUS # Буквы по умолчанию
-
+# Цвета
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 GREEN = (0, 255, 0)
 RED = (255,0,0)
 YELLOW = (255, 255, 0)
+
